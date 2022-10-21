bb3a1fdc736c474b5e8359bc7ec9b8fbf7fccf5b
yeah <kudoryi14@gmail.com>
2022-10-21 17:06:49 +0000

    python

---
+++ grade calc.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 15:11:16 2022
+
+@author: kudoryi
+"""
+
+
+grade=input("enter your grade:")
+if grade== "A":
+       print("excellent")
+elif grade=="B" :
+       print("very good")
+elif grade=="C" :
+       print(" good")
+elif grade=="D" :
+       print("fair")
+elif grade=="E" :
+       print("pass")
+elif grade=="F" :
+       print("FAIL")
+else: 
+       print("ENTER YOUR GRADE")
